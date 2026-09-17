a1ffa6a6fc68cb2d38d30182f1a860c246b335af
Umaru <kar24014@byui.edu>
2026-09-16 22:06:36 -0400

    update

---
+++ ponder/test.js
@@ -0,0 +1,18 @@
+const PI = 3.14;
+let radius = 3;
+let area = PI * radius * radius;
+console.log(area);
+const num1 = 1;
+const num2 = 1;
+console.log(num1 + num2);
+const num3 = 1;
+const num4 = 2;
+console.log(num3 + num4); 
+let course = "WDD 131";
+if (true) {
+    let student = "Umaru";
+    console.log("Course:", course);
+    console.log("Student:", student);
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
