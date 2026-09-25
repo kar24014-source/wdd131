7e2149dbf5aa16a50180622ba178c1afea97f0f6
Umaru <kar24014@byui.edu>
2026-09-24 21:50:12 -0400

    update after watching rubric

--- mission/script.js
+++ mission/script.js
@@ -1,28 +1,16 @@
 
 const selectElem = document.querySelector('select');
-const logo = document.querySelector('img');
 const body = document.body;
-const university = document.querySelector('.university');
-const line = document.querySelector('.line');
+const logo = document.querySelector('img');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
-    const current = selectElem.value;
-
-    if (current === 'dark') {
-        body.style.backgroundColor = '#111';
-        body.style.color = '#f5f5f5';
-        body.style.borderColor = '#444';
-        university.style.color = '#dbeafe';
-        line.style.borderTopColor = '#444';
+    if (selectElem.value === 'dark') {
+        body.classList.add('dark');
         logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png';
     } else {
-        body.style.backgroundColor = '#fff';
-        body.style.color = '#111';
-        body.style.borderColor = '#bbb';
-        university.style.color = '#1676bd';
-        line.style.borderTopColor = '#ddd';
+        body.classList.remove('dark');
         logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp';
     }
 }
